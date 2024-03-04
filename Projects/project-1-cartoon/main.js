ad76cbca990dc730e0f82617abdd89c1a077241c
Brian S <briansternfeld51@gmail.com>
2024-03-03 19:44:40 -0500

    starting project

--- Projects/project-1-cartoon/main.js
+++ Projects/project-1-cartoon/main.js
@@ -2,7 +2,6 @@
 
 // Note canvas size is 1500x800 pixels.
 
-
 const mainCanvas = document.getElementById("mainCanvas");
 const context = mainCanvas.getContext("2d");
 
@@ -15,7 +14,21 @@ function drawMoon(context, x, y) {
 }
 
 function drawStars(context, numOfStars) {
+    context.beginPath();
 
+    let i 
+    for (let i = 0; i < context.width; i++) {
+        for (let j = 0; j < context.height / 4; j++) {
+            var x = Math.random() * context.width;
+            var y = Math.random() * context.height / 4;
+            context.fillRect(x, y, 1, 1);
+        }    
+    }
+
+    // context.fillRect(100, 100, 1, 1);
+    // context.fillRect(200, 200, 1, 1);
+    context.fillStyle = "white";
+    context.fill();
 }
 
 function drawBuilding(context, x, y, length, width, buildingColor, windowColor) {
@@ -24,4 +37,12 @@ function drawBuilding(context, x, y, length, width, buildingColor, windowColor) 
 // Functions end.
 
 
-drawMoon(context, 1350, 110)
+drawMoon(context, 1350, 110);
+drawStars(context);
+
+var gradient = context.createLinearGradient(0, 0, mainCanvas.width, 0);
+gradient.addColorStop("0", "white");
+gradient.addColorStop("0.5", "red");
+context.fillStyle = gradient;
+context.font = "30px Verdana";
+context.fillText("City Scape In Progress", 100, 200);
