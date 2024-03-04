cc94abc5684f90a622f9fb3c252de6907423982d
Brian S <briansternfeld51@gmail.com>
2024-03-03 19:19:53 -0500

    starting project

--- Projects/project-1-cartoon/main.js
+++ Projects/project-1-cartoon/main.js
@@ -1,34 +1,27 @@
 "use strict";
 
-const canvas = document.getElementById("mainCanvas");
-const context = canvas.getContext("2d");
-
-context.fillStyle = "yellow";
-context.fillRect(20, 20, 20, 10);
-context.fillRect(20, 320, 20, 10);
-context.fillRect(460, 20, 20, 10);
-context.fillRect(460, 320, 20, 10);
+// Note canvas size is 1500x800 pixels.
 
 
+const mainCanvas = document.getElementById("mainCanvas");
+const context = mainCanvas.getContext("2d");
 
-context.beginPath();
-context.moveTo(250, 175);
-context.lineTo(50, 50);
-context.lineTo(450, 300);
-context.lineTo(450, 50);
-context.lineTo(50, 300);
-context.lineTo(50, 50);
-context.strokeStyle = "black";
-context.stroke();
-
-drawHouse(context, 0.5);
-
-function drawHouse(context, offset) {
-    // Draw top half triangle.
-    context.moveTo(250 + offset, 10 + offset);
-    context.lineTo(10 + offset, 60 + offset);
-    context.lineTo(490 + offset, 60 + offset);
-    context.lineTo(250 + offset, 10 + offset);
-    context.strokeStyle = "green";
-    context.stroke();
+// Functions start.
+function drawMoon(context, x, y) {
+    context.beginPath();
+    context.arc(x, y, 100, 0, 2 * Math.PI);
+    context.fillStyle = "gray";
+    context.fill();
 }
+
+function drawStars(context, numOfStars) {
+
+}
+
+function drawBuilding(context, x, y, length, width, buildingColor, windowColor) {
+    
+}
+// Functions end.
+
+
+drawMoon(context, 1350, 110)
