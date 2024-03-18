aa3ecda324384d59fbe1a9f96fa62b96f84f5e92
Brian S <briansternfeld51@gmail.com>
2024-03-17 20:23:08 -0400

    changed theme to camping

--- Projects/project-1-cartoon/main.js
+++ Projects/project-1-cartoon/main.js
@@ -6,6 +6,14 @@ const mainCanvas = document.getElementById("mainCanvas");
 const context = mainCanvas.getContext("2d");
 
 // Functions start.
+function drawWindow(canvas, x, y, radius) {
+    context.fillStyle = "white";
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2 * Math.PI);
+    context.rect(mainCanvas.width, 0, -mainCanvas.width, mainCanvas.height);
+    context.fill();
+}
+
 function drawMoon(context, x, y, radius) {
     context.beginPath();
     context.arc(x, y, radius, 0, 2 * Math.PI);
@@ -23,15 +31,6 @@ function drawMoon(context, x, y, radius) {
     }
 }
 
-
-function drawWindow(canvas, x, y, radius) {
-    context.fillStyle = "white";
-    context.beginPath();
-    context.arc(x, y, radius, 0, 2 * Math.PI);
-    context.rect(mainCanvas.width, 0, -mainCanvas.width, mainCanvas.height);
-    context.fill();
-}
-
 function drawStars(context, numOfStars) {
     context.fillStyle = "#0000ff";
     for (var i = 0; i < numOfStars; i++) {
@@ -46,16 +45,61 @@ function drawGround(context) {
     context.fillRect(1, mainCanvas.height / 2, mainCanvas.width, mainCanvas.height / 4);
 }
 
+function drawCabin(context, x, y, height, width) {
+  
+    // Draw cabin body.
+    context.fillStyle = "#483131"
+    context.fillRect(x, y, width, height);
+
+    // Draw the logs that make up the cabin walls.
+    var logHeight = 15;
+    context.beginPath();
+    for (var i = height + y; i >= y; i -= logHeight) {
+        context.moveTo(x, i);
+        context.lineTo(x + width, i);
+    }
+    context.strokeStyle = "#FFFFFF";
+    context.stroke();
+
+    // Draw a window.
+    context.fillStyle = "#FFBF00";
+    context.fillRect(x + width / 4, y + height / 4, width / 2, height / 2);
+    
+    // Draw the window frame.
+    context.beginPath();
+    context.lineWidth = 4;
+    context.strokeStyle = "#000000";
+    context.strokeRect(x + width / 4, y + height / 4, width / 2, height / 2);
+    context.lineWidth = 2;
+    context.moveTo(x + width / 2, y + height / 4);
+    context.lineTo(x + width / 2, y + (height / 4) * 3);
+    context.moveTo(x + width / 4, y + height / 2);
+    context.lineTo(x + (width / 4) * 3, y + height / 2);
+    context.stroke();
+
+    // Draw the roof.
+    
+
+}
+
+function drawCampFire() {}
+
+function drawDistantForest() {}
+
+function drawLake() {}
+
 // Functions end.
 
 drawGround(context);
 drawStars(context, 1000);
 drawMoon(context,  700, 100, 80);
+drawCabin(context, 100, 370, 200, 100);
+drawCabin(context, 10, 400, 250, 150);
 // drawWindow(context, 550, 550, 500);
 
 var gradient = context.createLinearGradient(0, 0, mainCanvas.width, 0);
-gradient.addColorStop("0", "blue");
-gradient.addColorStop("0.5", "black");
+gradient.addColorStop("0", "red");
+gradient.addColorStop("0.5", "white");
 context.fillStyle = gradient;
 context.font = "30px Verdana";
-context.fillText("Place Holder", 20, 40);
+context.fillText("Camp Site", 20, 40);
--- Projects/project-1-cartoon/testing/test.js
+++ Projects/project-1-cartoon/testing/test.js
@@ -18,19 +18,42 @@ function drawRandomStars(context, numOfStars) {
   }
 }
 
-function drawBuilding(context, x, y, height, width) {
-  context.fillStyle = "#808080";
-  context.fillRect(x, y, width, height);
-  context.fillStyle = "#ffffff";
+function drawCabin(context, x, y, height, width) {
+  
+    // Draw cabin body.
+    context.fillStyle = "#483131"
+    context.fillRect(x, y, width, height);
 
-  // for (var i = y + 10; y < height - 10; i += 10) {
-  //   for (var j = x + 10; j < width - 10; j += 10) {
-  //     context.fillRect(i, y, width / 8, height / 4);
-  //   }
-  // }
-  context.fillRect(260, 20, width / 16, height / 8);
+    // Draw the logs that make up the cabin walls.
+    var logHeight = 15;
+    context.beginPath();
+    for (var i = height + y; i >= y; i -= logHeight) {
+        context.moveTo(x, i);
+        context.lineTo(x + width, i);
+    }
+    context.strokeStyle = "#FFFFFF";
+    context.stroke();
+
+    // Draw a window.
+    context.fillStyle = "#FFBF00";
+    context.fillRect(x + width / 4, y + height / 4, width / 2, height / 2);
+    
+    // Draw the window frame.
+    context.beginPath();
+    context.lineWidth = 4;
+    context.strokeStyle = "#000000";
+    context.strokeRect(x + width / 4, y + height / 4, width / 2, height / 2);
+    context.lineWidth = 2;
+    context.moveTo(x + width / 2, y + height / 4);
+    context.lineTo(x + width / 2, y + (height / 4) * 3);
+    context.moveTo(x + width / 4, y + height / 2);
+    context.lineTo(x + (width / 4) * 3, y + height / 2);
+    context.stroke();
+
+    // Draw the roof.
+    
+
 }
-
 drawRandomStars(context, 2000);
-drawBuilding(context, 250, 10, 200, 150);
+drawCabin(context, 250, 10, 200, 150);
 
